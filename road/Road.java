d37b5680b1f1c73f18aa946246586feb7ae404f4
kishordkk <kishordk461@gmail.com>
2022-07-21 17:38:56 +0530

    modification

--- road/Road.java
+++ road/Road.java
@@ -1,8 +1,10 @@
 package road;
 
 public class Road {
+	
 	public void bike() {
 		System.out.println("Bike Suzuki");
+		System.out.println("task1gitbash");
 	}
 		public void car() {
 			System.out.println("Car Maruti");
